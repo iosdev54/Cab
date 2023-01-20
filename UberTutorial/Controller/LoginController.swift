5345052509ab0d79a11debd3bca9986dfb76a639
iosdev54 <iosdev54@gmail.com>
2023-01-20 17:09:20 +0200

    Add code to SceneDelegate for a launch without Storyboard. Create LoginController. Add Extension for constraints.

---
+++ UberTutorial/Controller/LoginController.swift
@@ -0,0 +1,43 @@
+//
+//  LoginController.swift
+//  UberTutorial
+//
+//  Created by Dmytro Grytsenko on 20.01.2023.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    //MARK: - Properties
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "UBER"
+        label.font = UIFont(name: "Avenir-light", size: 36)
+        label.textColor = UIColor(white: 1, alpha: 0.8)
+        return label
+    }()
+     
+    
+    
+    
+    //MARK: - Lifecycle
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(red: 25/255, green: 25/255, blue: 25/255, alpha: 1)
+        
+        view.addSubview(titleLabel)
+        
+        titleLabel.anchor(top: view.safeAreaLayoutGuide.topAnchor)
+        titleLabel.centerX(inView: view)
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+
+
+}
---
+++ UberTutorial/Utils/Extensions.swift
@@ -0,0 +1,54 @@
+//
+//  Extensions.swift
+//  UberTutorial
+//
+//  Created by Dmytro Grytsenko on 20.01.2023.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func anchor(top: NSLayoutYAxisAnchor? = nil,
+                left: NSLayoutXAxisAnchor? = nil,
+                right: NSLayoutXAxisAnchor? = nil,
+                bottom: NSLayoutYAxisAnchor? = nil,
+                paddingTop: CGFloat = 0,
+                paddingLeft: CGFloat = 0,
+                paddingRight: CGFloat = 0,
+                paddingBottom: CGFloat = 0,
+                width: CGFloat? = nil,
+                height: CGFloat? = nil) {
+       
+    translatesAutoresizingMaskIntoConstraints = false
+       
+        if let top = top {
+            topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+        }
+        if let left = left {
+            leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+        }
+        if let right = right {
+            rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+        }
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom).isActive = true
+        }
+        if let width = width {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        if let height = height {
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+        
+    }
+    
+    func centerX(inView view: UIView) {
+        centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+    
+    func centerY(inView view: UIView) {
+        centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+
+}
